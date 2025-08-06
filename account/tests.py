5733c4f190efa8f11915b5c17fad42a935291707
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-06 08:53:23 +0530

    22686

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from .models import TestCase
 # Create your tests here.
+
+class RestaurantModelTest(TestCase):
+    def test_restaurant_creation(self):
+        restaurant = Restaurant.object.create(name="Tasty Bites")
+        self.assertEqual(restaurant.name,"Tasty Bites")
+        
