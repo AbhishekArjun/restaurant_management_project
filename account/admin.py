a82b1c7ac528635ede7396fce4bca46073082a30
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-06 08:50:00 +0530

    22686

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+
+    def_str_(self):
+        return self.name
