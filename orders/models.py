f005630e428ef5bb65b03d6144e8dc5c0461a71a
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-06 08:46:37 +0530

    22685

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll_number = models.IntegerField()
+    email = models.EmailField()
+    enrolled_date = models.DataField(auto_now_add=True)
+    def_str_(self):
+        return self.name
