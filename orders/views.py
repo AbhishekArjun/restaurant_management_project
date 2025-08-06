fbea05f12ad99be6598208be01ff79fffce2d3c2
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-06 09:04:58 +0530

    22691

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .model import Restaurant
+
+def home_view(request):
+    restaurant = Restaurant.objects.first()
+    context = {
+        'restaurant_name': restaurant.name if restaurant else 'Restaurant',
+        'restaurant_phone': restaurant.phone if restaurant else 'N/A'
+
+    }
+    return render(request, 'home.html', context)
