f0886d38c8f5686300b1b6a07997ba1c53f8d431
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-06 09:30:56 +0530

    23212

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def contact_us(request):
-    retrun render(request, "home/contact.html")
+    retrun render(request, "reservations.html")
