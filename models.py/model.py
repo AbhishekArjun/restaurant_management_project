8e66652d1e590eb5abc2fe64ce116548bd233bb6
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-08 10:43:56 +0530

    26542

--- models.py/model.py
+++ models.py/model.py
@@ -1,6 +1,9 @@
 from django.sb import models
-class Restaurant(models.Model):
+class Menu(models.Model):
     name = models.CharField(max_length=100)
-    address = models.TextField()
+    description = models.TestField()
+    price = models.DeciamlField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+
     def _str_(self):
         return self.name
