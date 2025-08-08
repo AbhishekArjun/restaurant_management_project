6fb4c30b078cc14d345c1dca77f4a5979106dbad
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-08 09:32:15 +0530

    26540

--- models.py/model.py
+++ models.py/model.py
@@ -1,8 +1,6 @@
 from django.sb import models
-class MenuItem(models.Model):
+class Restaurant(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, deciaml_places=2)
-
-    def_str_(self):
-        retrun self.name
+    address = models.TextField()
+    def _str_(self):
+        return self.name
