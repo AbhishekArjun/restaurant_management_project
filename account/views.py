19c0cf31f122429af4843ad1b5b304fa7e48f694
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-08 10:22:02 +0530

    26541

--- account/views.py
+++ account/views.py
@@ -3,5 +3,5 @@ from .models import Restaurant
 # Create your views here.
 def home(request):
     restaurant = Restaurant.objects.first()
-    retrun render(request, 'homepage.html', { 'restaurant':restaurant})
+    return render(request, 'homepage.html', { 'restaurant':restaurant})
     
