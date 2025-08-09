c12abd34e1942c122edd2c155865ffa0abe5724c
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-09 20:08:53 +0530

    28082

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Restaurant
 # Create your views here.
 def home(request):
-    restaurant = Restaurant.objects.first()
-    return render(request, 'homepage.html', { 'restaurant':restaurant})
-    
+   cart = request.session.get('cart', [])
+   cart_count = len(cart)
+   return render(request, 'home.html', {'cart_count' : cart_count})
+   
