987cbc7c6c384fd65cf94defec8dd81aa77670f0
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-09 19:33:46 +0530

    28081

--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .model import Restaurant
+from .model import ContactForm
 
-def home_view(request):
-    restaurant = Restaurant.objects.first()
-    context = {
-        'restaurant_name': restaurant.name if restaurant else 'Restaurant',
-        'restaurant_phone': restaurant.phone if restaurant else 'N/A'
+def contact_view(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form,is_valid(){
+            name = form.cleaned_data.get("name")
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
 
-    }
-    return render(request, 'home.html', context)
-    
+            return render(request,"contact_success.html", {"name": name})
+            else:
+                form = ContactForm()
+
+                return render(request, "contact.html", {"form": form})
+        }
+
+
