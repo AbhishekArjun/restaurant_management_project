73f4034bc164792c529a51da6f6889cf0a5ba570
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-09 20:04:56 +0530

    28082

--- orders/views.py
+++ orders/views.py
@@ -10,7 +10,7 @@ def contact_view(request):
             name = form.cleaned_data.get("name")
             email = form.cleaned_data["email"]
             message = form.cleaned_data["message"]
-
+            
             return render(request,"contact_success.html", {"name": name})
             else:
                 form = ContactForm()
