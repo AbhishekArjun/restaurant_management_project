20150fa201872de5c7ae2c263e3f046e1bfc8442
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-11 12:09:46 +0530

    29587

---
+++ templates/breadcrumb_tages.py
@@ -0,0 +1,15 @@
+from django import template
+
+register =template.Library()
+@register.inclusion_tag('partials/breadcrumbs.html', takes_context=True)
+def breadcrumbs(context):
+    request = context['request']
+    path_parts = request.path.strip('/').split('/')
+    breadcrumbs_list - []
+    url_accumulator = ''
+
+    for part in path_parts:
+        url_accumulator += '/' + part
+        breadcrumbs_list.append({
+            
+        })
