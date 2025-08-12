605664b3d23144e12151570b9f649abd0bc17cd6
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-12 11:34:45 +0530

    29594

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('order/', views.order_page, name='order_page'),
+    path('order/', views.order_page, name='order_page')
     path('reservation/', views.reservations,name='reservation'),
 ]
