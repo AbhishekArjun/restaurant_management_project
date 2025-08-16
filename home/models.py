e4a4711cccb83fa912fe2cad1688076b3e039831
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-16 18:45:27 +0530

    34084

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class OpeningHours(models.Model):
+    day = models.CharField(max_length=20)
+    open_time = models.TimeField()
+    close_time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.day}: {self.open_time} - {self.close_time}"
