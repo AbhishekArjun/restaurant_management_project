896220959db363586c178441a671875bb7cdb570
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-19 10:44:56 +0530

    35579

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import * home_view
+from . import views
 
 urlpatterns = [
-    path('', home_view, name='home' .),
-    path('about/', views.about,name='about')
+    path("about-chef/", views.about_chef, name="about_chef"),
 ]
