437a25d5b86d645ce8a6d8e3d5504a9eb92eea9b
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-19 10:40:09 +0530

    35581

--- menu/views.py
+++ menu/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-ffrom .models import Menu
+from django.core.paginator import Paginator
+from .models import Menu
 
-def homepage(request):
-    query = request.GET.get('q', '')
-    if query:
-        items = Menu.objects.filter(name_icontainss=query)
-        else:
-            items = Menu.object.all()
-            return render(request, 'homepage.html', {'items'items, 'query'})
+def menu_view(request):
+    items = MenuItem.object.all().order_by("name")
+    paginator = Paginator(items, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "menu.html", {"page_obj": page_obj})
