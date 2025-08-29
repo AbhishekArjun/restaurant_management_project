9502c7a30cdbb8442a1df0144fc1206e008538ca
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-29 13:32:57 +0530

    40438

---
+++ menu/views.menu.py
@@ -0,0 +1,10 @@
+from .models import MenuItem
+
+def menu_view(request):
+    search_query = request.GET.get('search', '')
+    if search_query:
+        menu_items = MenuItem.objects.filter(name__icontains=search_query)
+        else:
+            menu_items = MenuItem.objects.all()
+            return render(request, "menu.html", {"menu_items": menu_items,
+            "search_query": search_query})
