5f3c0b6adc9ced48bdf20a720c653cb8d6bbdfdf
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-08-29 12:37:42 +0530

    40439

--- models.py/models.py
+++ models.py/models.py
@@ -3,14 +3,7 @@ from django.db import models
 class Restaurant(models.Model):
     name = modelsCharField(max_length=255, verbose_name="Restaurant Name")
     address = models.TextField(verbose_name="Address", 
-    help_text="Full postal address of the restaurant"
-    )
     
-  
-    created_at = models.DateTimeField(auto_now_add=True)
-    class Meta:
-        verbose_name = "Restaurant"
-        verbose_name_plural = "Restaurants"
         def  _str_(self):
             return self.name
             
