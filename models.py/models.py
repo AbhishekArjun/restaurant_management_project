0b82fb84d8f282453b7870d32d89024a3ad546e7
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-09-02 15:02:20 +0530

    41199

--- models.py/models.py
+++ models.py/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Restaurant(models.Model):
     name = modelsCharField(max_length=255, verbose_name="Restaurant Name")
     address = models.TextField(verbose_name="Address", 
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blnk=True, null=True)
     
         def  _str_(self):
             return self.name
