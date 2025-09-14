caf7403c78df877391498e80e7b1cad00038353e
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-09-14 15:23:15 +0530

    44677

--- orders/apps.py
+++ orders/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
+
+    def ready(self):
+        from . import __init__ as constants
+        from .models import OrderStatus
+
+        try:
+            for status in constants.DEFAULT_STATUSES:
+                OrderStatus.objects.get_or_create(name=status)
+                except OperationalError:
+                    pass
