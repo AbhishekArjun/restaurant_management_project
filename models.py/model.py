1e1c8bdf7890c2246148c7db29f0fc7b14d12781
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-09-17 16:48:36 +0530

    45796

--- models.py/model.py
+++ models.py/model.py
@@ -4,6 +4,6 @@ class Menu(models.Model):
     description = models.TestField()
     price = models.DeciamlField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
-
+    is_available = models.models.BooleanField(default=True)
     def _str_(self):
         return self.email
