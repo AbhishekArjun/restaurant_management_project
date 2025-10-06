b703d150ba9b9ca97e0f68b49aa5944eb0f924ed
AbhishekArjun <147574209+AbhishekArjun@users.noreply.github.com>
2025-10-06 12:16:30 +0530

    53612

--- orders/order.py
+++ orders/order.py
@@ -6,4 +6,4 @@ class OderManager(models.Manager):
         class Order(models.Model):
             status = models.CharField(max_length=20, choices=[
                 ('pending', 'Pending'),
-            ])
+            ]
